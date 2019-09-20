dd26036268266349ef951df36503628243fc4ce4
xiaoerque <xiaoerque@126.com>
2019-09-20 16:17:50 +0800

    实现代码生成

--- src/main/java/com/pcwl/demo/DemoApplication.java
+++ src/main/java/com/pcwl/demo/DemoApplication.java
@@ -1,13 +1,20 @@
 package com.pcwl.demo;
 
+import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.ServletComponentScan;
+import org.springframework.cache.annotation.EnableCaching;
+import org.springframework.transaction.annotation.EnableTransactionManagement;
 
+@MapperScan("com.pcwl.demo.*.dao")
+@EnableTransactionManagement
+@ServletComponentScan
 @SpringBootApplication
+@EnableCaching
 public class DemoApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(DemoApplication.class, args);
     }
-
 }
--- src/main/java/com/pcwl/demo/test/TestController.java
+++ src/main/java/com/pcwl/demo/test/TestController.java
@@ -5,7 +5,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 class TestController {
-    @RequestMapping("/index")
+    @RequestMapping("/test")
     public String index() {
         return "test";
     }
