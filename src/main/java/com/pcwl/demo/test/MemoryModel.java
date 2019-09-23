cd80a27daec8b973fcd6af808b2805a28bab7a99
xiaoerque <xiaoerque@126.com>
2019-09-23 14:28:36 +0800

    9.23

---
+++ src/main/java/com/pcwl/demo/test/MemoryModel.java
@@ -0,0 +1,13 @@
+package com.pcwl.demo.test;
+
+import java.util.HashMap;
+
+public class MemoryModel {
+    public static void main(String[] args) {
+        Runtime run = Runtime.getRuntime();//取得Runtime类的对象
+        System.out.println("MAX_MEMORY="+run.maxMemory());//系统分配的运行空间
+        System.out.println("TOTAL_MEMORY="+run.totalMemory());
+        System.out.println("FREE_MEMORY="+run.freeMemory());
+        //HashMap
+    }
+}
