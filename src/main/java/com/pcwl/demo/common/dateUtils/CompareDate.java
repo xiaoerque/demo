5031bfe2b08e50bde1b7cd3e587fb334ca0a1aa5
xiaoerque <xiaoerque@126.com>
2020-01-14 11:47:39 +0800

    日期的大小比较

--- src/main/java/com/pcwl/demo/common/dateUtils/CompareDate.java
+++ src/main/java/com/pcwl/demo/common/dateUtils/CompareDate.java
@@ -11,13 +11,15 @@ import java.util.Date;
 public class CompareDate {
     public static void main(String[] args) throws ParseException {
         Date now = new Date();
-        Date testTime = new SimpleDateFormat("yyyy-MM-dd").parse("2020-01-15");
-        System.out.println("当前的时间:"+ DateFormatUtils.format(now,"yyyy-MM-dd"));
-        System.out.println("测试的时间:"+ DateFormatUtils.format(testTime,"yyyy-MM-dd"));
+        Date testTime = new SimpleDateFormat("yyyy-MM-dd").parse("2020-01-14");
+        System.out.println("当前的时间:"+ DateFormatUtils.format(now,"yyyy-MM-dd HH:mm:ss"));
+        System.out.println("测试的时间:"+ DateFormatUtils.format(testTime,"yyyy-MM-dd HH:mm:ss"));
 
-        System.out.println("当前时间比测试时间:1----------->"+DateUtils.truncatedCompareTo(now,testTime,Calendar.YEAR));
-        System.out.println("当前时间比测试时间:2----------->"+DateUtils.truncatedCompareTo(now,testTime,Calendar.MONTH));
-        System.out.println("当前时间比测试时间:5----------->"+DateUtils.truncatedCompareTo(now,testTime,Calendar.DATE));
-        System.out.println("当前时间比测试时间:4----------->"+DateUtils.truncatedCompareTo(now,testTime,Calendar.HOUR));
+        System.out.println("当前时间比测试时间:YEAR----------->"+DateUtils.truncatedCompareTo(now,testTime,Calendar.YEAR));
+        System.out.println("当前时间比测试时间:MONTH----------->"+DateUtils.truncatedCompareTo(now,testTime,Calendar.MONTH));
+        System.out.println("当前时间比测试时间:DATE----------->"+DateUtils.truncatedCompareTo(now,testTime,Calendar.DATE));
+        System.out.println("当前时间比测试时间:HOUR----------->"+DateUtils.truncatedCompareTo(now,testTime,Calendar.HOUR));
+        System.out.println("当前时间比测试时间:MINUTE----------->"+DateUtils.truncatedCompareTo(now,testTime,Calendar.MINUTE));
+        System.out.println("当前时间比测试时间:SECOND----------->"+DateUtils.truncatedCompareTo(now,testTime,Calendar.SECOND));
     }
 }
